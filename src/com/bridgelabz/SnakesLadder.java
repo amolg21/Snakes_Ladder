751e9ea4599dd68d40136e27028d737b4fd47c0b
amol ghotale <amol9.ag@gmail.com>
2022-01-03 00:27:30 +0530

    ADDED: Game started

---
+++ src/com/bridgelabz/SnakesLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class SnakesLadder {
+
+    public static void main(String[] args) {
+	// Game of snakes and ladder
+        System.out.println("Game started with single player from inital point 0");
+    }
+}
