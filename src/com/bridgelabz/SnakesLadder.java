74c44110b14e5b4a1c79e597f199fb3a709bff6b
amol ghotale <amol9.ag@gmail.com>
2022-01-03 00:37:34 +0530

    ADDED: Die rolling

--- src/com/bridgelabz/SnakesLadder.java
+++ src/com/bridgelabz/SnakesLadder.java
@@ -3,7 +3,11 @@ package com.bridgelabz;
 public class SnakesLadder {
 
     public static void main(String[] args) {
-	// Game of snakes and ladder
+	// Game of snakes and ladder.
         System.out.println("Game started with single player from inital point 0");
+
+        //  Die is rolled to get a random value.
+        int die_value = (int)Math.floor(Math.random() * 10) % 7;
+        System.out.println("Value of die is " + die_value);
     }
 }
